13d31863772e938821fc3b23463f59bd327abcc2
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-19 03:05:00 -0500

    writing code to open camara application

---
+++ er1/Program.cs
@@ -0,0 +1,10 @@
+﻿int months = 12, bounus = 5000;
+double ratePerHour = 12.34;
+int numberOfHoursWorked = 10;
+
+double currentMonthWage = ratePerHour * numberOfHoursWorked + bounus;
+
+Console.WriteLine(currentMonthWage);
+
+ratePerHour += 2;
+Console.WriteLine(ratePerHour);
