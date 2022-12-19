307cf7e8d96645c4d2b1f55413b764200fda8291
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-19 04:46:29 -0500

    cloned solution code

--- er1/Program.cs
+++ er1/Program.cs
@@ -1,10 +1,44 @@
-﻿int months = 12, bounus = 5000;
-double ratePerHour = 12.34;
-int numberOfHoursWorked = 10;
+﻿/*using System.Runtime.InteropServices;
+//parameterless Main() methods
 
-double currentMonthWage = ratePerHour * numberOfHoursWorked + bounus;
 
-Console.WriteLine(currentMonthWage);
 
-ratePerHour += 2;
-Console.WriteLine(ratePerHour);
+partial class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+        int months = 12, bounus = 5000;
+        double ratePerHour = 12.34;
+        int numberOfHoursWorked = 10;
+
+        double currentMonthWage = ratePerHour * numberOfHoursWorked + bounus;
+
+        Console.WriteLine(currentMonthWage);
+
+        ratePerHour += 2;
+        Console.WriteLine(ratePerHour);
+
+
+
+        int hwnd = 0;
+        IntPtr hwndChild = IntPtr.Zero;
+
+        //Get a handle for the Calculator Application main window
+        hwnd = (int)FindWindow(null, "Calculator");
+        if (hwnd == 0)
+        {
+      *//*      if (MessageBox.Show("Couldn't find the calculator" +
+                               " application. Do you want to start it?",
+                               "TestWinAPI",
+                               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("Calc");
+            }*//*
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        static extern IntPtr FindWindow(string? lp1, string lp2);
+    }
+}*/
